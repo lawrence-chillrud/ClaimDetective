5c13ceb1635627d24df5dbc93e0c8bd29776afd5
lawrence-chillrud <lgc2139@columbia.edu>
2020-12-04 20:17:54 -0500

    Updated sample.py with one extra sentence.

--- sample.py
+++ sample.py
@@ -6,8 +6,7 @@ from claim_detective import *
 sherlock = ClaimDetective(path_to_model = "./models/claimbuster/model.pth")
 
 # put the sentences / document you would like to check for claims into a list of sentences:
-sentences = ["Global warming is causing increasing hurricanes.", "Cats are so much better than dogs."]
-
+sentences = ["Global warming is causing increasing hurricanes.", "Cats are so much better than dogs.", "This is the third sentence."]
 # pass the above list to the model's inspect() method:
 # inspect() will return a pandas DataFrame object with 3 columns: 
 # Sentence, Check-Worthiness Score, Prediction
@@ -17,7 +16,8 @@ sentences = ["Global warming is causing increasing hurricanes.", "Cats are so mu
 # check-worthy statements are 1's, non-check-worthy are 0's.
 # If labels is passed, then inspect() will print accuracy statistics & a confusion matrix
 # If you don't have labels, labels = None by default. 
-claims = sherlock.inspect(sents=sentences, labels = [1,0])
+labels = [1, 0, 0]
+claims = sherlock.inspect(sents=sentences, labels = labels)
 
 # The pandas DataFrame object returned by inspect() 
 # can then be saved to a .csv file with the report() method.
